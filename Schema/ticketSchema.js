adac270608f1e991a87ca0528e7e8c0d2f95412d
DuckoDas <100500099+DuckoDas@users.noreply.github.com>
2022-08-22 19:35:54 +0200

    Add files via upload

---
+++ Schema/ticketSchema.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+const ticketCreateSchema = new Schema({
+  guildId: String,
+  channelId: String,
+  categoryId: String, 
+  ticketlog: String,
+  supportRole: String,
+});
+
+module.exports = model("ticketSchema", ticketCreateSchema, "userTickets");
